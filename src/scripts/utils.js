57f7dc7bbb2187b11a019a67eaba2fa63134cd17
Aaron Eiche <aeiche@jaguarlandrover.com>
2021-06-12 17:44:01 -0700

    Adds latLongDistance

---
+++ src/scripts/utils.js
@@ -0,0 +1,44 @@
+/* 
+latLongDistance - Takes two lat/long values and returns distance
+Usage: 
+
+let distance = latLongDistance([39.8528, -75.1638],[39.8528, -75.2638]);
+
+
+Arguments: 
+latlong1 - An array consisting of 2 numerical values. Latitude, then longitude.
+latlong2 - An array consisting of 2 numerical values. Latitude, then longitude. 
+
+Note: Formula is based off of that found at https://www.movable-type.co.uk/scripts/latlong.html
+*/
+
+const latLongDistance = (latlong1, latlong2) => {
+    if( latlong1.length !== 2 || 
+        latlong2.length !== 2 ||
+        Array.isArray(latlong1) == false ||
+        Array.isArray(latlong2) == false        
+    ) {
+        throw new Error(`Arguments must be provided as 2 value arrays. Received ${JSON.stringify(latlong1)} and ${JSON.stringify(latlong2)}`);
+    }
+
+    /* 
+    Check for actual numerical values here: 
+    if(isNaN(Number(latlong[0]))) {
+        //throw error - numeric values please. 
+        // though we should convert strings that are valid numbers.
+    }
+    */
+    const radian1 = latlong1[0] * Math.PI/180;
+    const radian2 = latlong2[0] * Math.PI/180;
+    
+    const latDelta = (latlong1[0] - latlong2[0]) * Math.PI/180;
+    const longDelta = (latlong1[1] - latlong2[1]) * Math.PI/180;
+
+    const a = Math.sin(latDelta/2) * Math.sin(latDelta/2) + // Square the Sine
+              Math.cos(radian1) * Math.cos(radian2) * 
+              Math.sin(longDelta/2) * Math.sin(longDelta/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return 6371e3 * c; //The distance in meters.
+}
+
+export {latLongDistance}
